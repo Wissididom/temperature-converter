0ea8b6cc2bbe0099e3d8fa5f049c3c7103fd1439
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-12-12 23:34:23 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,145 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  Events,
+  PermissionsBitField,
+} from "discord.js";
+const bot = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+  partials: [
+    Partials.User,
+    Partials.Channels,
+    Partials.GuildMemeber,
+    Partials.Message,
+    Partials.Reaction,
+  ],
+});
+const token = process.env["TOKEN"];
+
+async function handleApplicationCommand(interaction) {
+  if (!interaction.isChatInputCommand()) return; // Not a chat input command. Needs to be revisited when we make message components or modals
+  let temp = null;
+  const { commandName } = interaction;
+  switch (commandName) {
+    case "temperatureconvert":
+      await interaction.deferReply({ ephemeral: true });
+      let sourceValue = interaction.options.getNumber("source");
+      let sourceFormat = interaction.options.getString("srcformat");
+      let targetFormat = interaction.options.getString("dstformat");
+      switch (sourceFormat) {
+        case "c":
+          switch (targetFormat) {
+            case "c":
+              await interaction.editReply({
+                content: `Converting ${sourceValue} °C to °C doesn't really make sense!`,
+              });
+              break;
+            case "f":
+              await interaction.editReply({
+                content: `${sourceValue} °C is ${sourceValue * 1.8 + 32} °F!`,
+              });
+              break;
+            case "k":
+              await interaction.editReply({
+                content: `${sourceValue} °C is ${sourceValue + 273.15} K!`,
+              });
+              break;
+            default:
+              await interaction.editReply({
+                content: "This combination of formats is not supported!",
+              });
+              break;
+          }
+          break;
+        case "f":
+          switch (targetFormat) {
+            case "c":
+              await interaction.editReply({
+                content: `${sourceValue} °F is ${(sourceValue - 32) / 1.8} °C!`,
+              });
+              break;
+            case "f":
+              await interaction.editReply({
+                content: `Converting ${sourceValue} °F to °F doesn't really make sense!`,
+              });
+              break;
+            case "k":
+              await interaction.editReply({
+                content: `${sourceValue} °F is ${
+                  (sourceValue - 32) / 1.8 + 273.15
+                } K!`,
+              });
+              break;
+            default:
+              await interaction.editReply({
+                content: "This combination of formats is not supported!",
+              });
+              break;
+          }
+          break;
+        case "k":
+          switch (targetFormat) {
+            case "c":
+              await interaction.editReply({
+                content: `${sourceValue} K is ${sourceValue - 273.15} °C!`,
+              });
+              break;
+            case "f":
+              await interaction.editReply({
+                content: `${sourceValue} K is ${
+                  (sourceValue - 273.15) * 1.8 + 32
+                } °F!`,
+              });
+              break;
+            case "k":
+              await interaction.editReply({
+                content: `Converting ${sourceValue} K to K doesn't really make sense!`,
+              });
+              break;
+            default:
+              await interaction.editReply({
+                content: "This combination of formats is not supported!",
+              });
+              break;
+          }
+          break;
+        default:
+          await interaction.editReply({
+            content: "This combination of formats is not supported!",
+          });
+          break;
+      }
+      break;
+    default:
+      await interaction.editReply({
+        content: "Error: Command does not exist! How did you even execute it?",
+        allowed_mentions: { parse: [] },
+      });
+      break;
+  }
+}
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error(reason, "Unhandled Rejection at Promise", promise);
+});
+process.on("uncaughtException", (err) => {
+  console.error(err, "Uncaught Exception thrown", err.stack);
+});
+
+bot.on(Events.ClientReady, async () => {
+  console.log(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on(Events.InteractionCreate, async (interaction) =>
+  handleApplicationCommand(interaction),
+);
+
+bot.login(token);
---
+++ setupCommands.js
@@ -0,0 +1,54 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import { REST, Routes, SlashCommandBuilder } from "discord.js";
+
+const token = process.env["TOKEN"];
+
+const rest = new REST().setToken(token);
+
+let commands = [
+  new SlashCommandBuilder()
+    .setName("temperatureconvert")
+    .setDescription("convert between different temperature formats")
+    .addNumberOption((option) =>
+      option
+        .setName("source")
+        .setDescription("The source value as double between -2^53 and 2^53")
+        .setRequired(true),
+    )
+    .addStringOption((option) =>
+      option
+        .setName("srcformat")
+        .setDescription("The source format to use")
+        .setRequired(true)
+        .addChoices(
+          { name: "°C", value: "c" },
+          { name: "°F", value: "f" },
+          { name: "K", value: "k" },
+        ),
+    )
+    .addStringOption((option) =>
+      option
+        .setName("dstformat")
+        .setDescription("The target format to use")
+        .setRequired(true),
+    ),
+];
+
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`,
+    );
+    const userData = await rest.get(Routes.user());
+    const userId = userData.id;
+    const data = await rest.put(Routes.applicationCommands(userId), {
+      body: commands,
+    });
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`,
+    );
+  } catch (err) {
+    console.error(err);
+  }
+})();
